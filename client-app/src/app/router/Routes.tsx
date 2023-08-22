5aab603c0cddaa61dcb10b02ffa1fc112aec048d
aleksandriprifti <prifti.aleksandri@gmail.com>
2023-08-23 00:16:58 +0200

    Add formik

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -3,21 +3,19 @@ import {
   RouteObject,
 } from "react-router-dom";
 import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import ManageElement from "../../features/activities/dashboard/ManageElement";
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <App />,
-    children:[
-      {path: 'activities', element: <ActivityDashboard />},
-      {path: 'activities/:id', element: <ActivityDetails />},
-      {path: 'createActivity', element: <ActivityForm />},
-      {path: 'manage/:id', element: <ActivityForm />}
+    children: [
+      { path: 'activities', element: <ActivityDashboard /> },
+      { path: 'activities/:id', element: <ActivityDetails /> },
+      { path: 'createActivity', element: <ActivityForm /> },
+      { path: 'manage/:id', element: <ActivityForm /> }
     ]
   }
 ]
